82f4dd31b30ee03e51a65fb419dafd52f8cf6dc1
TiagoSampaio-UTAD <tiagoalvessampaio12@gmail.com>
2026-02-08 02:16:40 +0000

    Correção: Nome do ficheiro Index.html com maiúscula

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const transporter = nodemailer.createTransport({
 
 // --- ROTA PRINCIPAL (Resolve o erro "Cannot GET /") ---
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'Index.html'));
 });
 
 // --- AUTENTICAÇÃO E UTILIZADORES ---
